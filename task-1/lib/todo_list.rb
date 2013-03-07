cc5738856710d04b3ebab66d19553696550dc9e7
gosia informatyk <lubiepracowac@gmail.com>
2013-03-07 16:54:09 +0100

    czesc zrobiona na zajeciach z todolist

--- task-1/lib/todo_list.rb
+++ task-1/lib/todo_list.rb
@@ -2,5 +2,26 @@ class TodoList
 
   # Initialize the TodoList with +items+ (empty by default).
   def initialize(items=[])
+    @items = items
   end
+
+  def empty?
+    @items.empty?
+  end
+
+  def size
+    if @items.empty?
+      0
+    else
+      @items.length
+    end
+  end	 
+  
+  def <<(item_descritpion)
+     @items.empty?
+  end
+  
+
+
+
 end
