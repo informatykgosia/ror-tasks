cdf0ba7917e96f7540a0eab886a186176657c1b6
gosia informatyk <lubiepracowac@gmail.com>
2013-03-14 09:08:40 +0100

    wkoncu wyszlo

--- task-1/lib/exceptions.rb
+++ task-1/lib/exceptions.rb
@@ -1,3 +1,4 @@
 # This exception is raised when illegal arguments are passed to methods.
 class IllegalArgument < Exception
+  0
 end
--- task-1/lib/todo_list.rb
+++ task-1/lib/todo_list.rb
@@ -2,5 +2,45 @@ class TodoList
 
   # Initialize the TodoList with +items+ (empty by default).
   def initialize(items=[])
+    @items=items
+    @list={}
+    if @items==nil
+     raise IllegalArgument
+    end
+  end
+ 
+  def empty?
+    @items.empty?
+  end
+
+  def complete(index)
+    @list[@items[index]]=true
+  end
+
+  def completed?(action)
+    true if @list[@items[action]] == true
+    
+  end
+
+  def uncompleted
+    @list[@items[index]]=false
+  end
+ 
+  def size
+     @items.size
+  end
+  
+  def <<(something)
+    @items << something
+    @list[something]=false
+    
+  end
+
+  def first
+    @items.first
+  end
+
+  def last
+    @items.last
   end
 end
