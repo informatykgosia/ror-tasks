108b853425b20c10455d2eccee223c978b08b117
gosia informatyk <lubiepracowac@gmail.com>
2013-03-26 22:04:38 +0100

    task-2 pierwsza czesc

--- task-2/lib/todo_list.rb
+++ task-2/lib/todo_list.rb
@@ -2,5 +2,44 @@ class TodoList
 
   # Initialize the TodoList with +items+ (empty by default).
   def initialize(items=[])
+    if items[:db] == nil
+      raise IllegalArgument
+    elsif
+      @database = items[:db]
+    end
+  end
+
+  def size
+    @database.items_count
+  end
+
+  def empty?
+    true if @database.items_count == 0
+  end
+
+  def << (other_object)
+    if other_object == nil
+      raise IllegalArgument  
+    elsif other_object[:title] == ""
+      nil  
+    else
+      @database.add_todo_item(other_object)
+    end
+  end
+
+  def first
+    @database.get_todo_item(0)
+  end
+
+  def last
+    @database.get_todo_item(self.size - 1)
+  end
+  
+  def toggle_state(index)
+    if @database.todo_item_completed?(index)
+      @database.complete_todo_item(0, true)
+    else
+      @database.complete_todo_item(index, false)
+    end
   end
 end
