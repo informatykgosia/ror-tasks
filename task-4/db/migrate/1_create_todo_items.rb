9206d9bca59ef2451a4318daa0722acd0eb80aec
gosia informatyk <lubiepracowac@gmail.com>
2013-06-05 20:51:16 +0200

    migracje zrobionie oh oh oh

---
+++ task-4/db/migrate/1_create_todo_items.rb
@@ -0,0 +1,10 @@
+class CreateTodoItems < ActiveRecord::Migration
+  def change
+    create_table :todo_items do |t|              
+      t.string :title
+      t.string :description
+      t.timestamp :date_due
+      t.integer :todo_list_id
+    end
+  end
+end 
